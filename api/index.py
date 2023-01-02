bd6717ca68111ddadf8ffa5f343d8f4fffeae084
ATACOM <120112713+ATACOM@users.noreply.github.com>
2023-01-02 08:44:05 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import requests
 import json
 app = Flask(__name__,
-            template_folder='templates',
+            template_folder='Templates',
             static_url_path='',
             static_folder='static')
 
